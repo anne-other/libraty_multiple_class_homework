5ee292d8a4716c69c3213660179b29cab566a5f0
anne-other <anne.c.ince@gmail.com>
2018-09-25 16:07:34 +0100

    Library can count the number of books it has

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class Library {
+    private ArrayList<Book> books;
+
+    public Library() {
+        this.books = new ArrayList<>();
+    }
+
+    public int bookCount() {
+        return this.books.size();
+    }
 }
