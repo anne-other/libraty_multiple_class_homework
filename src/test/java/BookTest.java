7b73edb36d4f669ab79d35a0289185695347d98c
anne-other <anne.c.ince@gmail.com>
2018-09-25 15:59:45 +0100

    Tests pass for book

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,11 +1,28 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class BookTest {
     Book book;
 
     @Before
     public void before(){
-        this.book = new Book();
+        this.book = new Book("The Lord Of The Rings", "J.R.R. Tolkein", "Fantasy");
     }
 
+    @Test
+    public void bookHasTitle() {
+        assertEquals("The Lord Of The Rings", book.getTitle());
+    }
+
+    @Test
+    public void bookHasAuthor() {
+        assertEquals("J.R.R. Tolkein", book.getAuthor());
+    }
+
+    @Test
+    public void bookHasGenre() {
+        assertEquals("Fantasy", book.getGenre());
+    }
 }
